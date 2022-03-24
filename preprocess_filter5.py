982ab8b1ad7c4a87a31592898918fe04b711d30f
Zhapy <43473143+SatoshiZpy@users.noreply.github.com>
2022-03-24 22:30:09 +0000

    Update preprocess_filter5.py

--- preprocess_filter5.py
+++ preprocess_filter5.py
@@ -18,7 +18,7 @@ random.seed(1234)
 workdir = 'datasets/'
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--dataset', default='Ciao', help='dataset name: Ciao/Epinions')
+parser.add_argument('--dataset', default='Yelp', help='dataset name: Ciao/Epinions')
 parser.add_argument('--test_prop', default=0.1, help='the proportion of data used for test')
 args = parser.parse_args()
 
@@ -49,14 +49,15 @@ item_count = 0
 rate_count = 0
 
 for s in click_f:
-	uid = s[0]
-	iid = s[1]
+	uid = s[1]
+	iid = s[2]
 	if args.dataset == 'Ciao':
 		label = s[3]
 	elif args.dataset == 'Epinions':
 		label = s[2]
 	else:
-		label = s[2]
+		label = s[3]
+		review_id = s[0]
 
 	if uid > user_count:
 		user_count = uid
@@ -64,17 +65,17 @@ for s in click_f:
 		item_count = iid
 	if label > rate_count:
 		rate_count = label
-	click_list.append([uid, iid, label])
+	click_list.append([review_id, uid, iid, label])
 
 pos_list = []
 for i in range(len(click_list)):
-	pos_list.append((click_list[i][0], click_list[i][1], click_list[i][2]))
+	pos_list.append((click_list[i][0], click_list[i][1], click_list[i][2], click_list[i][3]))
 
 # remove duplicate items in pos_list because there are some cases where a user may have different rate scores on the same item.
 pos_list = list(set(pos_list))
 
 # filter user less than 5 items
-pos_df = pd.DataFrame(pos_list, columns = ['uid', 'iid', 'label'])
+pos_df = pd.DataFrame(pos_list, columns = ["rid", 'uid', 'iid', 'label'])
 filter_pos_list = []
 user_in_set, user_out_set = set(), set()
 for u in tqdm(range(user_count + 1)):
@@ -83,9 +84,10 @@ for u in tqdm(range(user_count + 1)):
 		user_out_set.add(u)
 		continue
 	user_in_set.add(u)
+	u_r = hist["rid"].tolist()
 	u_items = hist['iid'].tolist()
 	u_ratings = hist['label'].tolist()
-	filter_pos_list.extend([(u, iid, rating) for iid, rating in zip(u_items, u_ratings)])
+	filter_pos_list.extend([(rid, u, iid, rating) for rid, iid, rating in zip(u_r, u_items, u_ratings)])
 print('user in and out size: ', len(user_in_set), len(user_out_set))
 print('data size before and after filtering: ', len(pos_list), len(filter_pos_list))
 
@@ -94,35 +96,35 @@ print('test prop: ', args.test_prop)
 pos_list = filter_pos_list
 
 random.shuffle(pos_list)
-num_test = int(len(pos_list) * args.test_prop)
-test_set = pos_list[:num_test]
-valid_set = pos_list[num_test:2 * num_test]
-train_set = pos_list[2 * num_test:]
+# num_test = int(len(pos_list) * args.test_prop)
+# test_set = pos_list[:num_test]
+# valid_set = pos_list[num_test:2 * num_test]
+# train_set = pos_list[2 * num_test:]
 
 
-print('Train samples: {}, Valid samples: {}, Test samples: {}, Total samples: {}'.format(len(train_set), len(valid_set), len(test_set), len(pos_list)))
+# print('Train samples: {}, Valid samples: {}, Test samples: {}, Total samples: {}'.format(len(train_set), len(valid_set), len(test_set), len(pos_list)))
 
 with open(workdir + args.dataset + '/dataset_filter5.pkl', 'wb') as f:
-	pickle.dump(train_set, f, pickle.HIGHEST_PROTOCOL)
-	pickle.dump(valid_set, f, pickle.HIGHEST_PROTOCOL)
-	pickle.dump(test_set, f, pickle.HIGHEST_PROTOCOL)
+	# pickle.dump(train_set, f, pickle.HIGHEST_PROTOCOL)
+	# pickle.dump(valid_set, f, pickle.HIGHEST_PROTOCOL)
+	# pickle.dump(test_set, f, pickle.HIGHEST_PROTOCOL)
+	pickle.dump(pos_list, f,  pickle.HIGHEST_PROTOCOL)
 
 
+pos_df = pd.DataFrame(pos_list, columns = ["rid", 'uid', 'iid', 'label'])
+# train_df = pd.DataFrame(train_set, columns = ["rid", 'uid', 'iid', 'label'])
+# valid_df = pd.DataFrame(valid_set, columns = ["rid", 'uid', 'iid', 'label'])
+# test_df = pd.DataFrame(test_set, columns = ["rid", 'uid', 'iid', 'label'])
 
-pos_df = pd.DataFrame(pos_list, columns = ['uid', 'iid', 'label'])
-train_df = pd.DataFrame(train_set, columns = ['uid', 'iid', 'label'])
-valid_df = pd.DataFrame(valid_set, columns = ['uid', 'iid', 'label'])
-test_df = pd.DataFrame(test_set, columns = ['uid', 'iid', 'label'])
-
-click_df = pd.DataFrame(click_list, columns = ['uid', 'iid', 'label'])
-train_df = train_df.sort_values(axis = 0, ascending = True, by = 'uid')
+click_df = pd.DataFrame(click_list, columns = ["rid", 'uid', 'iid', 'label'])
+# train_df = train_df.sort_values(axis = 0, ascending = True, by = 'uid')
 pos_df = pos_df.sort_values(axis = 0, ascending = True, by = 'uid')
 
 """
 u_items_list: 存储每个用户交互过的物品iid和对应的评分，没有则为[(0, 0)]
 """
 for u in tqdm(range(user_count + 1)):
-	hist = train_df[train_df['uid'] == u]
+	hist = pos_df[pos_df['uid'] == u]
 	u_items = hist['iid'].tolist()
 	u_ratings = hist['label'].tolist()
 	if u_items == []:
@@ -132,14 +134,14 @@ for u in tqdm(range(user_count + 1)):
 
 
 
-train_df = train_df.sort_values(axis = 0, ascending = True, by = 'iid')
+pos_df = pos_df.sort_values(axis = 0, ascending = True, by = 'iid')
 
 """
 i_users_list: 存储与每个物品相关联的用户及其评分，没有则为[(0, 0)]
 """
 userful_item_set = set()
 for i in tqdm(range(item_count + 1)):
-	hist = train_df[train_df['iid'] == i]
+	hist = pos_df[pos_df['iid'] == i]
 	i_users = hist['uid'].tolist()
 	i_ratings = hist['label'].tolist()
 	if i_users == []:
