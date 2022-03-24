a0f75890ab51d0b6948f582a6ab584a86e0f931b
Zhapy <43473143+SatoshiZpy@users.noreply.github.com>
2022-03-24 22:28:48 +0000

    Update dataloader.py

--- dataloader.py
+++ dataloader.py
@@ -4,25 +4,27 @@ import torch
 from torch.utils.data import Dataset
 
 class GRDataset(Dataset):
-	def __init__(self, data, u_items_list, u_users_list, u_users_items_list, i_users_list):
-		self.data = data
+	def __init__(self, data, u_items_list, u_users_list, u_users_items_list, i_users_list, review_list):
+		self.users = data["user_id"].tolist()
+		self.items = data["item_id"].tolist()
+		self.labels = data["label"]
 		self.u_items_list = u_items_list
 		self.u_users_list = u_users_list
 		self.u_users_items_list = u_users_items_list
 		self.i_users_list = i_users_list
-		
-
+		self.review_list = review_list
 
 	def __getitem__(self, index):
-		uid = self.data[index][0]
-		iid = self.data[index][1]
-		label = self.data[index][2]
+		rid = self.review_list[index]
+		uid = self.users[rid]
+		iid = self.items[rid]
 		u_items = self.u_items_list[uid]
 		u_users = self.u_users_list[uid]
 		u_users_items = self.u_users_items_list[uid]
 		i_users = self.i_users_list[iid]
+		label = self.labels[rid]
 
-		return (uid, iid, label), u_items, u_users, u_users_items, i_users
+		return (uid, iid, label), u_items, u_users, u_users_items, i_users, rid
 
 	def __len__(self):
-		return len(self.data)
+		return len(self.review_list)
